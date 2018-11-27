e19f0337dd37821c99ac708d93aacee910ec3aca
Marino Bonetti <marinobonetti@gmail.com>
2018-11-27 14:54:16 +0100

    refactoring

--- app/assets/config/alchemy_custom_model_manifest.js
+++ app/assets/config/alchemy_custom_model_manifest.js
@@ -1,2 +1,2 @@
-//= link_directory ../javascripts/alchemy/custom/model .js
-//= link_directory ../stylesheets/alchemy/custom/model .css
+//= link_directory ../javascripts/alchemy-custom-model .js
+//= link_directory ../stylesheets/alchemy-custom-model .css
--- app/assets/javascripts/alchemy-custom-model/total_page_elfinder.js.coffee
+++ app/assets/javascripts/alchemy-custom-model/total_page_elfinder.js.coffee
@@ -1,4 +1,4 @@
-#= require jquery
+#= require jquery.js
 #= require jquery-ui
 # require admin/elFinder
 
